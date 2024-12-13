15955f6fb44bc7e4c169ea2faa4226b6782a822e
Ilyosbek0088 <ilyosbek0088@gmail.com>
2024-12-13 21:07:59 +0500

    asd

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/navbar'
+import Main from './main'
+import Footer from './footer'
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Main/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <button className='button-N1'></button>
+  )
+}
+
+export default Button
